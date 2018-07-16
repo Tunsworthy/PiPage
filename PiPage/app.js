819aa48233d16dda790b96b3d2daa20789d2f14e
Tom Unsworth <tomunsworth@Toms-MacBook-Pro.local>
2018-07-16 23:03:57 +0100

    Flash message working on its own button... tyring to get rpio working also.

--- PiPage/app.js
+++ PiPage/app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var flash = require('connect-flash')
+var session = require('express-session');
+var flash = require('express-flash');
+var rpio = require('rpio');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var sessionStore = new session.MemoryStore;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,9 +20,18 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cookieParser('secret'));
+app.use(session({
+    cookie: { maxAge: 60000 },
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}));
+
+app.use(flash());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -33,23 +45,27 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 var ajax = require('./routes/ajax');
 app.post('/ajax', ajax);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+ //catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-app.use(flash());
+// Route that creates a flash message using the express-flash module
+app.all('/express-flash', function( req, res ) {
+    req.flash('success', 'This is a flash message using the express-flash module.');
+    res.redirect(301, '/');
+});
+
 
 
 module.exports = app;
--- PiPage/routes/index.js
+++ PiPage/routes/index.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var rpio = require('rpio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pi Page' });
+  res.render('index', {title: 'Pi Page', expressFlash: req.flash('success'), sessionFlash: res.locals.sessionFlash });
+});
+
+router.all('/indexflash/:gpio/:status', function(req, res,next){
+     console.log(req.params)
+    //rpio.write(2,low)
+    req.flash('success', 'Index Flash');
+    res.redirect(301, '/');
 });
 
 module.exports = router;
+
+/*
+$(function(){               
+    $('.gpio-button').on('click', function(e,req,res){       
+            var data = {};
+            data.action = "write";
+            data.gpio = $(this).data("gpio");
+            data.status = $(this).data("status");
+            console.log(data);
+            $.ajax({
+                type: 'POST',
+                data: JSON.stringify(data),
+                contentType: 'application/json',
+                url: '/ajax',                       
+                error: function(data) {
+                    alert("Error");
+                },
+                succes: function(data) {
+                    alert("succes");
+                }
+        });
+    });             
+});
+
+/* GET ajax response. 
+module.exports = function(req, res) {
+    if (req.body.hasOwnProperty('action')) {
+        switch(req.body.action) {
+            case 'write':
+                rpio.init({mapping: 'gpio'});
+                rpio.open(req.body.gpio, rpio.OUTPUT, + req.body.status);
+                rpio.write(req.body.gpio, + req.body.status);
+                res.contentType('json');
+                res.send({ gpio: req.body.gpio, status: req.body.status });
+            break;
+        }
+      
+    }
+};          
+*/
