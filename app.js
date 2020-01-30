8ddb19ac2cce9804480289134939d0786cd1b253
Tom Unsworth <mail@tomunsworth.net>
2020-01-30 21:36:36 +1100

    gpio updates

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var session = require('express-session');
 var flash = require('express-flash');
 var rpio = require('rpio');
+var bodyParser = require('body-parser');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
