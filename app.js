80a1ca92dbda54c64d42b6f7781e0ac5e3c799aa
Tom Unsworth <mail@tomunsworth.net>
2020-01-30 21:34:03 +1100

    gpio updates

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
