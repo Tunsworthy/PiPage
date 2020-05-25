1fbb3afb5f922033354ab377963aefd920e7f0aa
Tom Unsworth <mail@tomunsworth.net>
2020-05-25 15:48:42 +1000

    test

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ app.all('/relay/', function(req, res,next){
     //1 = low
     //change the Params into int
     var action = {};
-        action.status = (+req.query.status);
-        action.gpio = (+req.query.gpio);
+        action.status = req.query.status;
+        action.gpio = req.query.gpio;
         gpiochange(action);
     if (action.status === 0) {
         req.flash('info', 'Power on sent');
@@ -26,13 +26,15 @@ app.all('/relay/', function(req, res,next){
     if (action.status === 1) {
         req.flash('info', 'Power Off');
     }
+    console.log(rpio.read(req.query.gpio));
     res.json("data")
+
 });
 
 }
 
 function gpiochange(action){
-    rpio.open(action.gpio, rpio.OUTPUT, rpio.HIGH);
+    rpio.open(action.gpio, rpio.OUTPUT);
     //Send action (on or off)
     rpio.write(action.gpio,action.status);
     console.log('write action ' + (rpio.read(action.gpio) ? 'high' : 'low'));    
